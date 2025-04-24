44ea4a961e01eb56ec96f29b0757dc9cc4429b4d
Travis Johnson <travis@thisguy.codes>
2025-04-24 00:28:43 -0400

    try 1

---
+++ go.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -euo pipefail
+
+echo "Installing Lix..."
+curl -sSf -L https://install.lix.systems/lix | sh -s -- install
+
+echo "Lix installed"
+
+echo "Cloning nix config repo..."
+mkdir -p ~/.config
+git clone https://github.com/ThisGuyCodes/nix-home ~/.config/nix-darwin
+
+echo "Installing darwin-rebuild..."
+nix run nix-darwin/master#darwin-rebuild -- switch
+
+echo "Go!"
+darwin-rebuild switch --flake ~/.config/nix-darwi
